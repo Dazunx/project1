b09280eeeff2ae891be28b1b990d0f5a43df7ef2
Dazunx <22100493.handong.ac.kr>
2022-09-13 10:43:09 +0900

    Update start method

--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -28,18 +28,33 @@ public class WordManager {
 	}
 	
 	public void start() {
-		
+		wordCRUD.loadFile();
 		while(true) {
 			int menu = selectMenu();
 			if(menu == 0) {
 				System.out.println("프로젝트 종료! 다음에 만나요~");
 				break;
 			}
-			if(menu == 4) {
-				wordCRUD.addWord();
-			}
 			else if(menu == 1) {
 				wordCRUD.listAll();
+			}
+			else if(menu == 2) {  
+				wordCRUD.searchLevel();
+			}
+			else if(menu == 3) {  
+				wordCRUD.searchWord();
+			}
+			else if(menu == 4) {
+				wordCRUD.addItem();
+			}
+			else if(menu == 5) {  // update
+				wordCRUD.updateItem();
+			}
+			else if(menu == 6) {  // delete
+				wordCRUD.deleteItem();
+			}
+			else if(menu == 7) {  // file
+				wordCRUD.saveFile();
 			}
 		}
 		
